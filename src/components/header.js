6f6ab81c08f80cdf79097c6da13df8fb87b1f626
Arshita <118299386+ArshitaKango@users.noreply.github.com>
2024-08-06 21:50:45 +0530

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -4,7 +4,7 @@ import vas from "../images/troll-face.png";
 function Header() {
   return (
     <header className='header'>
-        <img className='header-image' src={vas} />
+        <img className='header-image' src={vas} alt="memeLogo"/>
         <h2 className='header-title'>Meme Generator</h2>
 
         <h4 className='header-project'>Create Your Own Memes</h4>
